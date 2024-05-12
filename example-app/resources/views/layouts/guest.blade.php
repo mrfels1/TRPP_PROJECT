8514734f372218e146aa4f106aa0c6fac3c6329b
BloodMistress <alevtinamironovaAM20041005love@gmail.com>
2024-05-12 18:35:08 +0300

    ccfbfxbxf

--- example-app/resources/views/layouts/guest.blade.php
+++ example-app/resources/views/layouts/guest.blade.php
@@ -13,7 +13,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js','resources/css/NavStyle.css'])
+</head> @vite(['resources/css/app.css', 'resources/js/app.js']) -->
 
 
         

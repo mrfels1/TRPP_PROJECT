7e23540483da8ff7dedb077b92670ec1de052894
Dmitrii <dimas9.00@mail.ru>
2024-05-12 16:58:03 +0300

    adding style

--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -82,63 +82,66 @@
 </aside>
 
 {{-- Вывод всех постов из запроса. Управляется из web.php "route /posts" --}}
+<div class="user-posts">
+    @foreach($posts as $post)
+    <div class="post">
+        <h2 class="title">
+            <a href="/post/{{$post->id}}">{{$post->title}}</a>
+        </h2>
+        <h3 class="text_content">
+            Автор: {{$post->getUserName()}}
+        </h3>
+        <h3 class="center">
+            @foreach($post->getAllTags() as $tag)
+            <span>{{$tag->text_content}}</span>
+            @endforeach
+        </h3>
 
-@foreach($posts as $post)
-<div class="post">
-    <h2 class="title">
-        <a href="/post/{{$post->id}}">{{$post->title}}</a>
-    </h2>
-    <h3 class="text_content">
-        Автор: {{$post->getUserName()}}
-    </h3>
-    <h3 class="center">
-        @foreach($post->getAllTags() as $tag)
-        <span>{{$tag->text_content}}</span>
-        @endforeach
-    </h3>
+        <p class="text_content">
+            {{$post->text_content}}
+        </p>
+        @if (Auth::check())
+        <div class="center table">
 
-    <p class="text_content">
-        {{$post->text_content}}
-    </p>
-    @if (Auth::check())
-    <div class="center table">
+            <div class="upvote_cnt">
+                <form method="POST" action="/post/{{$post->id}}/like">
+                    @csrf
+                    <button type="submit"
+                        style="{{$post->isLikedBy(auth()->user())?'color: green;' : 'color: black;'}}">
+                        [▲] {{$post->likes ?:0}}
+                    </button>
+                </form>
+            </div>
 
-        <div class="upvote_cnt">
-            <form method="POST" action="/post/{{$post->id}}/like">
-                @csrf
-                <button type="submit" style="{{$post->isLikedBy(auth()->user())?'color: green;' : 'color: black;'}}">
-                    [▲] {{$post->likes ?:0}}
-                </button>
-            </form>
+            <div class="downvote_cnt">
+                <form method="POST" action="/post/{{$post->id}}/like">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"
+                        style="{{$post->isDislikedBy(auth()->user())?'color: green;' : 'color: black;'}}">
+                        [▼] {{$post->dislikes ?:0}}
+                    </button>
+                </form>
+            </div>
+            <div class="comment_cnt" style="color: black;">
+                Комм: {{$post->comments ?:0}}</div>
+
         </div>
+        @else
+        <div class="center table">
 
-        <div class="downvote_cnt">
-            <form method="POST" action="/post/{{$post->id}}/like">
-                @csrf
-                @method('DELETE')
-                <button type="submit" style="{{$post->isDislikedBy(auth()->user())?'color: green;' : 'color: black;'}}">
-                    [▼] {{$post->dislikes ?:0}}
-                </button>
-            </form>
+            <div class="upvote_cnt" style="color: black; ">
+                [▲] {{$post->likes ?:0}}</div>
+            <div class="downvote_cnt" style="color: black;">
+                [▼] {{$post->dislikes ?:0}}</div>
+            <div class="comment_cnt" style="color: black;">
+                Комм: {{$post->comments ?:0}}</div>
+
         </div>
-        <div class="comment_cnt" style="color: black;">
-            Комм: {{$post->comments ?:0}}</div>
-
+        @endif
     </div>
-    @else
-    <div class="center table">
-
-        <div class="upvote_cnt" style="color: black; ">
-            [▲] {{$post->likes ?:0}}</div>
-        <div class="downvote_cnt" style="color: black;">
-            [▼] {{$post->dislikes ?:0}}</div>
-        <div class="comment_cnt" style="color: black;">
-            Комм: {{$post->comments ?:0}}</div>
-
-    </div>
-    @endif
+    @endforeach
 </div>
-@endforeach
 @else
 <p>No posts</p>
 @endunless
